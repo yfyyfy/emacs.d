68c48952457e6c2a3e4666dd8febbdae6cb7ef81
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2018-06-23 17:52:23 +0900

    [fix_package_management] Enforce my-package load on calling my-elpamr-restore-from-mirror

--- init.el
+++ init.el
@@ -23,7 +23,6 @@
 
 (when (and (not (file-directory-p package-user-dir))
 	   (yes-or-no-p (format "package-user-dir (%s) does not exist. Restore from local mirror? " package-user-dir)))
-  (require 'my-package)
   (require 'my-elpa-mirror)
   (my-elpamr-restore-from-mirror nil t))
 
