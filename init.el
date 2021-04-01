204f5b365d6fba78eef643e5eaf74a0b2bd24e24
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2021-04-01 12:03:38 +0900

    [company_configuration] Add configuration for company-mode

--- init.el
+++ init.el
@@ -790,6 +790,44 @@
      (define-key yas-keymap [(tab)] nil)
      (yas/global-mode 1)))
 
+;; Company
+(autoload 'company-mode-on "company" nil t)
+(with-eval-after-load 'company
+  ;; Select-next/previous with meta, not ctrl.
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  ;; Show doc with M-d, not C-h.
+  (define-key company-active-map (kbd "C-h") nil)
+  (define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
+  ;; Complete if only one option is available, otherwise select next.
+  (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+  ;; Etc.
+  (setq company-minimum-prefix-length 2)
+  (setq company-idle-delay 0.1)
+  (setq company-selection-wrap-around t)
+  ;; Faces
+  (set-face-attribute 'company-tooltip nil
+                      :foreground "black"
+                      :background "lightgray")
+  (set-face-attribute 'company-preview-common nil
+                      :foreground "dark gray"
+                      :background "black"
+                      :underline t)
+  (set-face-attribute 'company-tooltip-selection nil
+                      :background "steelblue"
+                      :foreground "white")
+  (set-face-attribute 'company-tooltip-common nil
+                      :foreground "black"
+                      :underline t)
+  (set-face-attribute 'company-tooltip-common-selection nil
+                      :foreground "white"
+                      :background "steelblue"
+                      :underline t)
+  (set-face-attribute 'company-tooltip-annotation nil
+                      :foreground "red"))
+
 ;; Personal utils
 (autoload 'my-diff-buffers "my-diff" nil t)
 (autoload 'my-grep "my-grep" nil t)
