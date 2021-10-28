96d65511106df69121ddb228799c247180cc6fbe
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2021-10-29 00:12:14 +0900

    Disable diff-hl-dired on remote dired buffer

--- init.el
+++ init.el
@@ -278,7 +278,7 @@
 
 ;; Diff-hl
 (defun my-dired-mode-hook ()
-  (diff-hl-dired-mode)
+  (diff-hl-dired-mode-unless-remote)
   (unless (display-graphic-p)
     (diff-hl-margin-mode)))
 (add-hook 'dired-mode-hook #'my-dired-mode-hook)
