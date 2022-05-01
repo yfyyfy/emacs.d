0a989476bfc87197c93d8179592df5fbf98ce1fd
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2022-05-01 14:37:49 +0900

    Add options to manual installation of lsp-eslint server

--- lisp/my-lsp.el
+++ lisp/my-lsp.el
@@ -6,17 +6,17 @@
 	(lsp-ui-doc--hide-frame))
     (lsp-ui-doc-mode 1)))
 
-(defun my-lsp-eslint-install ()
+(defun my-lsp-eslint-install (&optional install-path no-message)
   "Insall server for lsp-eslint.
 lsp-install-server for lsp-eslint@lsp-mode-8.0.0 won't work (fixed in 20220425.1046)."
   (let ((url "https://github.com/emacs-lsp/lsp-server-binaries/blob/master/dbaeumer.vscode-eslint-2.2.2.vsix?raw=true")
-	(install-path (f-join lsp-server-install-dir "eslint-patch/unzipped"))
+	(install-path (or install-path (f-join lsp-server-install-dir "eslint-patch/unzipped")))
 	(download-path (make-temp-file "ext" nil ".zip")))
     (if (f-exists? install-path)
-	(message "vscode-eslint is already installed in %s. Check if it is copied or symlinked to %s" install-path lsp-eslint-unzipped-path)
+	(or no-message (message "vscode-eslint is already installed in %s. Check if it is copied or symlinked to %s" install-path lsp-eslint-unzipped-path))
       (url-copy-file url download-path t)
       (lsp-unzip download-path install-path)
       (f-delete download-path)
-      (message "Installed vscode-eslint in %s. Copy or symlink the directory to %s" install-path lsp-eslint-unzipped-path))))
+      (or no-message (message "Installed vscode-eslint in %s. Copy or symlink the directory to %s" install-path lsp-eslint-unzipped-path)))))
 
 (provide 'my-lsp)
